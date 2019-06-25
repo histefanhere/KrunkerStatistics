6ae7df37d888366621d17744aee632c18241711d
Histefanhere <stefan.zdra@gmail.com>
2019-06-25 21:19:30 +1200

    You're welcome Alex :)

--- commands/random/dice_roll.js
+++ commands/random/dice_roll.js
@@ -6,14 +6,17 @@ class DiceRollCommand extends commando.Command {
             name: 'roll',
             group: 'random',
             memberName: 'roll',
-            description: 'Rolls a die'
-            // details: "more descriptive description of the command"
-            // examples: ['roll']
+            description: 'Rolls a die.',
+            details: 'Oh come on, surely you know what rolling a die does.',
+            examples: ['roll']
         });
     }
 
     async run(message, args) {
         var roll = Math.floor(Math.random() * 6) + 1;
+        if (message.author.id === '216336989635608576') {
+            roll = 6;
+        }
         message.reply("You rolled a " + roll);
     }
 }
